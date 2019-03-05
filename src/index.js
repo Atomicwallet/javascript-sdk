fbc08244cc7896d1eedbe95918248c523a711b90
pitersky <1749310+pitersky@users.noreply.github.com>
2019-03-05 14:13:33 +0300

    ledger dependency commented out

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import * as client from "./client"
 import * as crypto from "./crypto"
 import * as amino from "./encoder"
 import * as utils from "./utils"
-import Ledger from "./ledger"
+// import Ledger from "./ledger"
 
 const { BncClient } = client
 module.exports = BncClient
 module.exports.crypto = crypto
 module.exports.amino = amino
 module.exports.utils = utils
-module.exports.ledger = Ledger
+// module.exports.ledger = Ledger
