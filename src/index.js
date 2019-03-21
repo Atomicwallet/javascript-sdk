b051d1424dac346397dadf43c8b7cdddd63fb97d
wave <wind@wave>
2019-03-21 20:35:34 +0300

    exclude amino dep

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import "babel-polyfill"
 import * as client from "./client"
 import * as crypto from "./crypto"
-import * as amino from "./encoder"
+// import * as amino from "./encoder"
 import * as utils from "./utils"
 // import Ledger from "./ledger"
 
 const { BncClient } = client
 module.exports = BncClient
 module.exports.crypto = crypto
-module.exports.amino = amino
+// module.exports.amino = amino
 module.exports.utils = utils
 // module.exports.ledger = Ledger
